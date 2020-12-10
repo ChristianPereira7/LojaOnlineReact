8799b904816ed95da2c5b08006b874a1a6804d49
Christian Pereira <christianpbsd@gmail.com>
2020-12-10 20:20:06 -0300

    Update

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,15 +1,41 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { SearchArea, PageArea } from './styled';
+import useApi from '../../helpers/LojaAPI'
+import { PageContainer} from '../../components/MainComponents';
+
+
 
 const Page = () => {
-    return(
-        <div>
-            <h1>Página inicial</h1>
 
-            <Link to="/about">Sobre</Link>
-        </div>
+    const api = useApi();
+
+    return (
+        <>
+            <SearchArea>
+                <PageContainer>
+                    <div className="searchBox">
+                        <form method="POST" action="/ads">
+                            <input type="text" name="q" placeholder="O que você procura?"/>
+                            <select name="state">
+
+                            </select>
+                            <button>Pesquisar</button>
+                        </form>
+                    </div>
+                    <div className="categoryList">
+
+                    </div>
+                </PageContainer>
+            </SearchArea>
+
+           <PageContainer>
+                <PageArea>
+                  ...
+                </PageArea>
+           </PageContainer>
+        </>
+       
     );
 }
-
 
 export default Page;
