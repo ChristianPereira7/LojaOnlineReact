b97ceedcf6cfc240156ea4191396983ad16f1659
Christian Pereira <christianpbsd@gmail.com>
2020-12-21 19:15:32 -0300

    Ads

--- src/Routes.js
+++ src/Routes.js
@@ -10,6 +10,7 @@ import NotFound from './pages/NotFound';
 import SignUp from './pages/SignUp';
 import AdPage from './pages/AdPage';
 import AddAd from './pages/AddAd';
+import Ads from './pages/Ads';
 
 
 
@@ -38,6 +39,10 @@ export default () => {
 
             <RouteHandler private exact path="/post-an-ad"> 
                 <AddAd/>
+            </RouteHandler>
+
+            <RouteHandler exact path="/ads"> 
+                <Ads/>
             </RouteHandler>
 
 
