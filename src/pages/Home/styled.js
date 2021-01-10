b9b2992e5ef26060cdedf5057f8ca9d6ac33d973
Christian Pereira <christianpbsd@gmail.com>
2021-01-10 17:42:48 -0300

    Home responsive

--- src/pages/Home/styled.js
+++ src/pages/Home/styled.js
@@ -74,6 +74,27 @@ padding: 20px 0;
     }
 }
 
+@media (max-width: 600px) {
+    .searchBox form {
+        flex-direction: column;
+
+        input{
+            padding: 10px;
+            margin-right: 0;
+            margin-bottom: 10px;
+        }
+        select{
+            width: 100%;
+            margin-bottom: 10px;
+        }
+    }
+
+    .categoryList .categoryItem {
+        width: 50%;
+        padding: 10px;
+    }
+}
+
 
 `;
 
@@ -96,5 +117,20 @@ h2{
     font-weight: bold;
     display: inline-block;
     margin-top: 10px;
+}
+
+@media (max-width: 600px){
+    
+    & {
+        margin-left: 10px;
+    }
+
+    h2{
+       text-align: center;
+    }
+
+    .list .aditem{
+        width: 100%;
+    }
 }
 `;
